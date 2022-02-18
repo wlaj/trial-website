596e6351ed20a6962386ea458ec6f1bdc131bce6
Lucas Jansen <wlajansen16@gmail.com>
2022-02-18 11:59:56 +0100

    Show GraphQL data on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    import { gql } from 'graphql-request'
+    import { client } from '$lib/graphql-client'
+  
+    export const load = async () => {
+      const query = gql`
+        query GetProjects {
+          projects {
+            name
+            slug
+            description
+            demo
+            sourceCode
+            image {
+              url
+            }
+          }
+        }
+      `
+  
+      const { projects } = await client.request(query)
+  
+      return {
+        props: {
+          projects,
+        },
+      }
+    }
+  </script>
+  
+  <script>
+    export let projects
+  </script>
+  
+  <pre>{JSON.stringify(projects, null, 2)}</pre>
